d6aea7c23975a808dfff7a0822c4bb968709ecd4
Gregor Sturm <mail@gregor-sturm.de>
2021-06-05 15:44:56 +0200

    Add auth-min-user-id

--- rstudio-conda/init2.sh
+++ rstudio-conda/init2.sh
@@ -8,6 +8,7 @@ echo rsession-ld-library-path=${CONDAENV}/lib >> /etc/rstudio/rserver.conf
 echo "R_LIBS_USER=${CONDAENV}/lib/R/library" > /home/rstudio/.Renviron
 # Set root password (with podman, we need to login as root rather than "rstudio")
 echo "root:$PASSWORD" | chpasswd
+echo "auth-minimum-user-id=0" >> /etc/rstudio/rserver.conf
 
 # Custom settings
 echo "session-timeout-minutes=0" > /etc/rstudio/rsession.conf
