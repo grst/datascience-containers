8e839abd47579aef45379fb48714c7382c6cf6f5
Gregor Sturm <mail@gregor-sturm.de>
2021-06-05 15:07:07 +0200

    document podman

--- rstudio-conda/init2.sh
+++ rstudio-conda/init2.sh
@@ -6,6 +6,8 @@ export RETICULATE_PYTHON=${CONDAENV}/bin/python
 echo rsession-which-r=${RSTUDIO_WHICH_R} > /etc/rstudio/rserver.conf
 echo rsession-ld-library-path=${CONDAENV}/lib >> /etc/rstudio/rserver.conf
 echo "R_LIBS_USER=${CONDAENV}/lib/R/library" > /home/rstudio/.Renviron
+# Set root password (with podman, we need to login as root rather than "rstudio")
+echo "root:$PASSWORD" | chpasswd
 
 # Custom settings
 echo "session-timeout-minutes=0" > /etc/rstudio/rsession.conf
